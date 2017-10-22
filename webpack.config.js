b2bfa75377b130d4042786e4089fb8407eced989
itaditya <adityaa803@gmail.com>
2017-10-22 16:42:01 +0530

    made separate css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "client", "index.js"),
@@ -6,28 +7,25 @@ module.exports = {
     filename: path.join("client", "dist", "bundle.js")
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["es2015"]
-        }
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        loader: "style-loader!css-loader"
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader",
+      query: {
+        presets: ["es2015"]
       }
-    ]
+    }, {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      use: ExtractTextPlugin.extract({
+        use: ["css-loader", "sass-loader"]
+      })
+    }]
   },
   resolve: {
     modules: [__dirname + "/node_modules"]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin(path.join("client", "dist", "style.css")),
+  ]
 };
