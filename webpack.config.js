d672741ee8201dd0d865fa651099b3a24a2cab9c
itaditya <adityaa803@gmail.com>
2017-11-23 16:31:58 +0530

    sourcemaps added

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,31 @@ const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require("webpack");
 
-
-module.exports = (env) => {
+module.exports = env => {
   return {
     entry: path.join(__dirname, "client", "index.js"),
     output: {
       filename: path.join("client", "dist", "app.js")
     },
-    devtool: env === 'development' ? 'eval-cheap-module-source-map' : false,
+    devtool: env === "development" ? "eval-cheap-module-source-map" : false,
     module: {
-      rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          presets: ["es2015"]
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          query: {
+            presets: ["es2015"]
+          }
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ExtractTextPlugin.extract({
+            use: ["css-loader", "sass-loader"]
+          })
         }
-      }, {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          use: ["css-loader", "sass-loader"]
-        })
-      }]
+      ]
     },
     resolve: {
       modules: [__dirname + "/node_modules"]
@@ -40,5 +42,5 @@ module.exports = (env) => {
         }
       })
     ]
-  }
+  };
 };
